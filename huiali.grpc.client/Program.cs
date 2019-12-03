c495a9087a510f602e6f02506d9bce0ba8f4bf11
huiali <huiali@hotmail.com>
2019-12-03 15:17:50 +0800

    netcore 3.0 grpc

---
+++ huiali.grpc.client/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+
+using huiali.grpc.server;
+
+namespace huiali.grpc.client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+           var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client =  new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "huiali" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            //Console.ReadKey();
+        }
+    }
+}
